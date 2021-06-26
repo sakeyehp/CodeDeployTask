00266685417dcc383f8140abe5edd608fb990a2d
unknown <Administrator@ABP-DIG-W3457.accessbankplc.com>
2021-06-26 13:12:08 +0100

    add example endpoint

--- src/main/java/com/sha/codedeploytask/CodeDeployTaskApplication.java
+++ src/main/java/com/sha/codedeploytask/CodeDeployTaskApplication.java
@@ -2,6 +2,8 @@ package com.sha.codedeploytask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class CodeDeployTaskApplication {
@@ -10,4 +12,13 @@ public class CodeDeployTaskApplication {
 		SpringApplication.run(CodeDeployTaskApplication.class, args);
 	}
 
+}
+
+@RestController
+class Hello {
+
+    @RequestMapping("/geostore/rest")
+    String index() {
+        return "This is a bootstrapped sample site using start.spring.io to demo the Geostore example";
+    }
 }
